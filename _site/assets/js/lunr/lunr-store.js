89bf9538395f25d5e588a2334e94eabd3b1e1597
Puneet Gupta <samparkpuneet@gmail.com>
2018-12-15 01:07:15 +0530

    First semi-complete post titanic

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,6 @@
-var store = []
+var store = [{
+        "title": "Customary Titanic Project: Binary  Classification",
+        "excerpt":"Classifying survivors in Titanic The customary binary classification problem for people who want to start with Machine learning. Introduction: aaa Classifying survivors: aaa Importing Libraries: importing libraries # linear algebra import numpy as np # data processing import pandas as pd # data visualization import seaborn as sns %matplotlib inline...","categories": [],
+        "tags": ["Machine Learning","Random Forest","Data Science","Kaggle"],
+        "url": "http://localhost:4000/titanic/",
+        "teaser":null}]
