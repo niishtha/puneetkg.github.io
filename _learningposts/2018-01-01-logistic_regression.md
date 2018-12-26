c83d2ff6e9310d482485d5ed7e80efd57762043d
Puneet Gupta <samparkpuneet@gmail.com>
2018-12-27 01:34:06 +0530

    correcting the images

--- _learningposts/2018-01-01-logistic_regression.md
+++ _learningposts/2018-01-01-logistic_regression.md
@@ -3,7 +3,7 @@ title: "Understanding Logistic regression in Python"
 date: 2018-01-01
 tags: [Machine Learning,Data science]
 header:
-  overlay_image: "/images/logistic-regression-example.jpg"
+  image: "/images/logistic.png"
 excerpt: "Machine Learning, Data Science, Binary Classification, Kaggle"
 mathjax: true
 ---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,23 @@
 var store = [{
-        "title": "learning pages",
-        "excerpt":"is this working ?  ","categories": [],
+        "title": "Understanding Decision trees in Python",
+        "excerpt":"Coming Soon !!  ","categories": [],
         "tags": ["Machine Learning","Data science"],
-        "url": "http://localhost:4000/learningposts/2018-01-01-learning/",
+        "url": "http://localhost:4000/learningposts/2018-01-01-decision_tree/",
+        "teaser":null},{
+        "title": "Understanding Linear Regression in Python",
+        "excerpt":"Coming Soon!!  ","categories": [],
+        "tags": ["Machine Learning","Data science"],
+        "url": "http://localhost:4000/learningposts/2018-01-01-linear_regression/",
+        "teaser":null},{
+        "title": "Understanding Logistic regression in Python",
+        "excerpt":"Coming Soon !!  ","categories": [],
+        "tags": ["Machine Learning","Data science"],
+        "url": "http://localhost:4000/learningposts/2018-01-01-logistic_regression/",
+        "teaser":null},{
+        "title": "Understanding random forests in Python",
+        "excerpt":"Coming Soon !!  ","categories": [],
+        "tags": ["Machine Learning","Data science"],
+        "url": "http://localhost:4000/learningposts/2018-01-01-random_forest/",
         "teaser":null},{
         "title": "Customary Titanic Project: Binary  Classification",
         "excerpt":"Classifying survivors in Titanic The customary binary classification problem for people who want to start with Machine learning. Introduction: Kaggle has created a number of competitions designed for beginners. The most popular of these competitions, and the one we’ll be looking at, is about predicting which passengers survived the sinking...","categories": [],
