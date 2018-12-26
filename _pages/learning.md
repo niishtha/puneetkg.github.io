4555b698520d2c1857f0f990d5c7dd5946bfe08c
Puneet Gupta <samparkpuneet@gmail.com>
2018-12-27 00:22:08 +0530

    changes from Romi complting all the things grouping posts into collections, changing the layout into for ML projects

--- _pages/learning.md
+++ _pages/learning.md
@@ -7,14 +7,6 @@ header:
  overlay_image: "/images/header_4.jpg"
 ---
 
-
-{% include group-by-array collection=site.learningposts field="tags" %}
-
-{% for tag in group_names %}
-  {% assign learningposts = group_items[forloop.index0] %}
-  <h2 id="{{ tag | slugify }}" class="archive__subtitle">{{ tag }}</h2>
-
-  {% for post in learningposts %}
-    {% include archive-single.html %}
-  {% endfor %}
+{% for post in site.learningposts %}
+  {% include archive-single.html %}
 {% endfor %}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "learning pages",
+        "excerpt":"is this working ?  ","categories": [],
+        "tags": ["Machine Learning","Data science"],
+        "url": "http://localhost:4000/learningposts/2018-01-01-learning/",
+        "teaser":null},{
         "title": "Customary Titanic Project: Binary  Classification",
         "excerpt":"Classifying survivors in Titanic The customary binary classification problem for people who want to start with Machine learning. Introduction: Kaggle has created a number of competitions designed for beginners. The most popular of these competitions, and the one we’ll be looking at, is about predicting which passengers survived the sinking...","categories": [],
         "tags": ["Machine Learning","Random Forest","Data Science","Kaggle"],
